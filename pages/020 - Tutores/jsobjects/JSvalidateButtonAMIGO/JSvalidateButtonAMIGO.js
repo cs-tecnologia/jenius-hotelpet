54f5b286dcb5fb12fd09721e24715001390d684c
Claudio Souza <claudio@cstecnologia.com.br>
2026-05-05 10:04:31 +0000

    feat: incluir modal conveniencia

--- pages/020 - Tutores/jsobjects/JSvalidateButtonAMIGO/JSvalidateButtonAMIGO.js
+++ pages/020 - Tutores/jsobjects/JSvalidateButtonAMIGO/JSvalidateButtonAMIGO.js
@@ -1,6 +1,6 @@
 export default {
 	// 1. Verifica se houve mudanças e se os campos obrigatórios estão preenchidos
-podeSalvarCONV: () => {
+podeSalvarAMIGO: () => {
     const action = appsmith.store.modalContexto?.acaoTipo;
     const row = Table1Amigos.selectedRow || {}; // Proteção caso a linha suma
 
@@ -29,7 +29,7 @@ podeSalvarCONV: () => {
 },
 	
 // 2. Função para o botão CANCELAR refatorada
-	resetFormCONV: async () => {
+	resetFormAMIGO: async () => {
 		// 1. Mudamos o estado de volta para "Visualizar/Editar" 
 		// Isso faz com que o Default Value do Input aponte para a Table1Amigos.selectedRow
 		await storeValue("modalContexto", { 
@@ -50,12 +50,12 @@ podeSalvarCONV: () => {
 	},
 	
 // Nova função para o botão CANCELAR
-podeCancelarCONV: () => {
+podeCancelarAMIGO: () => {
     const action = appsmith.store.modalContexto?.acaoTipo;
     
     // Chamamos a função de verificação de mudanças que já criamos
     // Importante: use o nome do seu JSObject antes do ponto
-    const houveAlteracao = JSvalidateButtonAMIGO.temAlteracaoCONV(); 
+    const houveAlteracao = JSvalidateButtonAMIGO.temAlteracaoAMIGO(); 
 
     if (action === "ADICIONAR") {
         // No modo ADICIONAR, o botão sempre funciona (true)
@@ -67,7 +67,7 @@ podeCancelarCONV: () => {
 },
 
 	// 1. Função que checa se o que está no input é diferente da tabela
-temAlteracaoCONV: () => {
+temAlteracaoAMIGO: () => {
     const contexto = appsmith.store.modalContexto?.acaoTipo;
     
     // Dados dos Inputs/Selects (O que o usuário está fazendo agora)
@@ -114,4 +114,21 @@ temAlteracaoCONV: () => {
 // Imprime o valor no console do Appsmith
 
 },
+
+	// 4. Ações de Banco (Delete e Reset)
+	deleteAMIGO: async function() {
+		JSutils.resetInactivityTimer();
+		const idExcluir = Table1Amigos.selectedRow.amigo001_id;
+
+		if (!idExcluir) return showAlert("Selecione um registro.", "warning");
+
+		try {
+			await DeletePetAmigos.run({ amigo001_id: idExcluir }); // Nome da query corrigido
+			showAlert("Excluído com sucesso!", "success");
+			await storeValue('amigoSelecionado', {});
+			await SelectAmigos.run(); // Nome da query de listagem corrigido
+		} catch (error) {
+			showAlert("Erro ao excluir.", "error");
+		}
+	}
 }
